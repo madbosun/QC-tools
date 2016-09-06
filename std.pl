0f64c88cda45efc3b1e4c84184c48e0de49d933c
Jason Byrd <byrd.jason@ensco.com>
2016-09-06 11:07:20 -0400

    std.pl should be gaussian output

--- std.pl
+++ std.pl
@@ -6,23 +6,22 @@
 @sym = qw(X H He Li Be B C N O F Ne Na Mg Al Si P S Cl Ar K Ca Sc Ti Y Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr Rb Sr Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I Xe Cs Ba La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu Hf Ta W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn);
 
 while(<>) {
-    if (/^ NSERCH/) {$searchline = $_;}
-    if (/COORDINATES OF ALL ATOMS ARE/) {
+    if (/Standard orientation:/) {
         print if $dbg;
-        $_ = <>; $_ = <>;
+        $_ = <>; $_ = <>; $_ = <>; $_ = <>;
         $i = 0;
         while(<>) {
-            last if(/^\n/);
-            $_ =~ s/^ //;
-            $line[$i++] = $_;
+            last if(/-------------------/);
+            ($ian,$x[$i],$y[$i],$z[$i]) = (split)[1,3,4,5];
+            $s[$i++] = $sym[$ian];
+            printf "%-4s %12s %12s %12s\n", $sym[$ian], $x, $y, $z if $dbg;
         }
     }
 }
 
-print " \$data\n";
-print $searchline;
-print " C1\n";
+print "struct\n";
+print "$i\n";
 for($k=0; $k < $i; $k++) {
-print $line[$k]
+    printf "  %-4s %12s %12s %12s\n", $s[$k], $x[$k], $y[$k], $z[$k];
 }
-print " \$end\n\n";
+print "\n";
